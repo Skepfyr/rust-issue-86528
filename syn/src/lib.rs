f1397bffe669216ecb5cc3841066048973cfaa13
Jack Rickard <jack.rickard@outlook.com>
2021-06-28 00:54:24 +0100

    Save

--- syn/src/lib.rs
+++ syn/src/lib.rs
@@ -1,3 +1,9 @@
+use std::str::FromStr;
+
 pub fn foo() {
-    "".parse().map(|_f: f64| ());
+    parse("").map(|_f: f64| ());
+}
+
+pub fn parse<F: FromStr>(this: &str) -> Result<F, F::Err> {
+    FromStr::from_str(this)
 }
