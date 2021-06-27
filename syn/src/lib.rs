6b629a23dcb5551af7733c0ac7cd6780361607b3
Jack Rickard <jack.rickard@outlook.com>
2021-06-28 00:53:04 +0100

    Save

--- syn/src/lib.rs
+++ syn/src/lib.rs
@@ -1,10 +1,3 @@
-#![allow(unused_variables, unused_mut, clippy::new_without_default)]
-use proc_macro2::Literal;
-
-pub struct LitInt {}
-
-pub fn foo() -> LitInt {
-    let token = "".parse().ok().map(Literal::f64_unsuffixed).unwrap();
-
-    LitInt {}
+pub fn foo() {
+    "".parse().map(|_f: f64| ());
 }
